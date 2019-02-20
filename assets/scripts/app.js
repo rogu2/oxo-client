1e4997f601ab864a8f843bf459830021f365dc7d
rogu2 <rguempel@gmail.com>
2019-02-20 14:35:33 -0500

    Add authHandler and gameHandler functions to app.js
    -Add handlers to app.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,22 +4,12 @@ const authEvents = require('./auth/events.js')
 const gameEvents = require('./game/events.js')
 
 $(() => {
-  // authEvents.addHandlers()
-  // gameEvents.addHandlers()
-  // authEvents
   $('#game-table').hide()
   $('#change-password-form').hide()
   $('#sign-out-button').hide()
   $('#restart-button').hide()
   $('#show-games-button').hide()
 
-  $('#sign-up-form').on('submit', authEvents.onSignUp)
-  $('#sign-in-form').on('submit', authEvents.onSignIn)
-  $('#sign-out-button').on('click', authEvents.onSignOut)
-  $('#change-password-form').on('submit', authEvents.onChangePassword)
-
-  // gameEvents -> probably to change
-  $('.box').on('click', gameEvents.onBoxClick)
-  $('#restart-button').on('click', gameEvents.onNewGame)
-  $('#show-games-button').on('click', gameEvents.onShowGames)
+  authEvents.addHandlers()
+  gameEvents.addHandlers()
 })
